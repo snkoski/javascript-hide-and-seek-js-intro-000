810a4aa4f24a33dd98e4c1ab4f6eb5be4bb5c022
Shawn Koski <shawnnkoski@gmail.com>
2018-03-20 12:05:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(`${selector}`);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let list = document.getElementById('app').querySelectorAll('div#grand-node');
+  let text = list[4].innerHTML;
+  return text;
+  
+}
