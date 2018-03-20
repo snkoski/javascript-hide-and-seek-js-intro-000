ebdd514260d1aa8744feedecff4cc5fa251fc594
Shawn Koski <shawnnkoski@gmail.com>
2018-03-20 12:45:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function deepestChild() {
   debugger;
   let list = document.getElementById('app').querySelectorAll('div#grand-node');
   //let text = list[4].innerHTML;
-  if (list.hasChildNode()) {
+  if (list.hasChildNodes()) {
     console.log("There is a child there");
   }
   for (let i = 0; i < list.length; i++) {
