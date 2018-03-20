5aef631bb494c9367487b316c5a41646eaca0cea
Shawn Koski <shawnnkoski@gmail.com>
2018-03-20 12:12:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,16 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
+  debugger;
   let list = document.getElementById('app').querySelectorAll('div#grand-node');
-  let text = list[4].innerHTML;
-  return text;
+  //let text = list[4].innerHTML;
+  for (let i = 0; i < list.length; i++) {
+    //console.log(list[0].innerHTML)
+    console.log('list length: ' + list.length);
+    console.log('test in for');
+  }
+  console.log(list[0][0][0][1].innerHTML)
+  console.log('test');
+  return list;
   
 }
